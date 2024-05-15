5b9f21d0fea2f5bf5d5110fc67e9664bff8f87ea
Suaiki <suaiki@Naphatthamons-MacBook-Air.local>
2024-05-15 12:05:45 +0700

    first commit jsd3 ex-2

---
+++ script.js
@@ -0,0 +1,9 @@
+const isEven = function (number) {
+  return number % 2 === 0;
+};
+console.log(isEven(5));
+
+// ### Exercise 2.2 converts the function from Exercise 2.1 into an arrow function</br>
+const isEven1 = (number) => number % 2 === 0;
+
+console.log(isEven(6));
